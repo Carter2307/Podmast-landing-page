10b27709bb36c7a535473db59732e58b4b28cafc
Roger Bentcha <remirjunior1999@gmail.com>
2025-09-25 20:49:26 +0200

    feat: Création du composant Header

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,12 +1,15 @@
-import { Logo } from '../atoms/Logo.tsx'
-import { NavMenu } from '../molecules/NavMenu.tsx'
-import { NAV_LINKS } from '../../data/links.ts'
-import { HamburgerMenu } from '../atoms/HamburgerMenu.tsx'
-import { Button } from '../atoms/Button.tsx'
-import { useState } from 'react'
+import { Logo } from '../atoms/Logo.tsx';
+import { NavMenu } from '../molecules/NavMenu.tsx';
+import { NAV_LINKS } from '../../data/links.ts';
+import { HamburgerMenu } from '../atoms/HamburgerMenu.tsx';
+import { Button } from '../atoms/Button.tsx';
+import { useState } from 'react';
+import { useWindowSize } from '@uidotdev/usehooks';
 
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
+  const { width } = useWindowSize();
+  const isLgScreen = (width || 0) >= 1080;
 
   return (
     <header className="header">
@@ -15,10 +18,13 @@ const Header = () => {
 
       <div className={'header__cta'}>
         <Button variant={'primary'}>Contact</Button>
-        <HamburgerMenu handleChange={(state) => setOpenMenu(state)} />
+
+        {!isLgScreen && (
+          <HamburgerMenu handleChange={(state) => setOpenMenu(state)} />
+        )}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
